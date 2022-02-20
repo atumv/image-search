db015047d8654c8896ac4ce0ac0c1d8298db6015
atumv <atumv.gm@gmail.com>
2022-02-20 12:00:51 +0700

    Updates

---
+++ precache-manifest.c8ef6f22c2477700c050c0b6cda9a0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f47280c6968f7b23ca93445275bac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa26e520e70a6bedc2b",
+    "url": "/static/css/main.f4574d35.chunk.css"
+  },
+  {
+    "revision": "d68b1fdf5d8394264664",
+    "url": "/static/js/2.52599d00.chunk.js"
+  },
+  {
+    "revision": "49ce7d95cc41f749170ec1525062f46d",
+    "url": "/static/js/2.52599d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa26e520e70a6bedc2b",
+    "url": "/static/js/main.be346cf2.chunk.js"
+  },
+  {
+    "revision": "14f717e6a006d5ecb110",
+    "url": "/static/js/runtime-main.36736485.js"
+  }
+]);
